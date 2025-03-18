ea2e550fb370c2544480537fe6e4aca5da71a449
Alex Marcovych <alexmarcovych@gmail.com>
2025-03-18 16:29:05 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ export const App = () => {
   const [selectedItem, setSelectedItem] = useState(goods[8]);
 
   const handleClear = () => {
-    setSelectedItem(null);
+    setSelectedItem('');
   };
 
   return (
